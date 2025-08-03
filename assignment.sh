a1a0c51df5ebc8c670e8672d85bc295b7bf60d0f
juan-d-bueno <jdbuenoi@gmail.com>
2025-08-03 17:13:45 -0400

    Juan Bueno committing 1st assignment

---
+++ assignment.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -x
+
+############################################
+# DSI CONSULTING INC. Project setup script #
+############################################
+# This script creates standard analysis and output directories
+# for a new project. It also creates a README file with the
+# project name and a brief description of the project.
+# Then it unzips the raw data provided by the client.
+
+if [ -d newproject ]; then
+  echo "Directory 'newproject' already exists. Please remove it before running this script."
+  exit 1
+fi
+mkdir newproject
+cd newproject
+
+mkdir analysis output
+touch README.md
+touch analysis/main.py
+
+# download client data
+curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+unzip -q rawdata.zip
+
+###########################################
+# Complete assignment here
+
+# 1. Create a directory named data
+mkdir data
+# 2. Move the ./rawdata directory to ./data/raw
+mkdir data/raw && mv rawdata data/raw
+# 3. List the contents of the ./data/raw directory
+ls data/raw
+# 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+for i in server_logs user_logs event_logs; do 
+mkdir -p data/processed/"$i"
+done
+# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp data/raw/rawdata/*server*.log data/processed/server_logs
+# 6. Repeat the above step for user logs and event logs
+cp data/raw/rawdata/*user*.log data/processed/user_logs
+cp data/raw/rawdata/*event*.log data/processed/event_logs
+
+# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm data/raw/rawdata/*ipaddr* ; rm data/processed/user_logs/*ipaddr*
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+find data/processed -type f > data/inventory.txt
+
+###########################################
+
+echo "Project setup is complete!"
